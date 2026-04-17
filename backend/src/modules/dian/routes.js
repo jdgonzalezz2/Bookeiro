b1a2ec68172cb7208eb4853daaad6035918a0b5c
jdgonzalezz2 <jd.gonzalez2@uniandes.edu.co>
2026-04-17 10:48:57 -0500

    delete

--- backend/src/modules/dian/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('./controller');
-const { authenticateToken } = require('../../middlewares/auth');
-
-router.post('/emitir', authenticateToken, controller.emitirFactura);
-router.get('/:transaccionId', authenticateToken, controller.getFactura);
-
-module.exports = router;
